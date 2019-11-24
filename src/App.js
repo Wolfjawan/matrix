20753e39d8b4ae8029069d56c6f4fb4412d2261a
Wolfjawan <mohsen000069@gmail.com>
2019-11-24 18:30:26 +0000

    first commit amd build matrix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import _ from "lodash";
+import { oppositeNumber, shuffle, symbols } from "./Helper";
+let w = window.innerWidth;
+const timesMatrix = () => {
+  const matrix = [];
+  var i;
+  for (i = 0; i < w / 30; i++) {
+    const fontSize = _.random(10, 40);
+    const animationDelay = `${fontSize / 10}s`;
+     // specifies the speed for each line depend on the font size to show how far is a line
+    const animationDuration = `${oppositeNumber(fontSize) + 10}s`;
+    // specifies the opacity for the color depend on the font size to show how far is a line
+    const color = `rgba(0, 233, 50, ${fontSize / 40})`;
+    matrix.push(
+      <pre
+        style={{
+          fontSize: `${fontSize}px`,
+          animationDelay,
+          animationDuration,
+          color
+        }}
+      >
+        {shuffle(symbols)}
+      </pre>
+    );
+  }
+  return matrix;
+};
+export default () => <section className="matrix">{timesMatrix()}</section>;
